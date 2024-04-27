600fd31a262afae8f44b4b96ec2c7169c5ffcb4c
suvenrj <99804277+suvenrj@users.noreply.github.com>
2024-04-27 16:16:23 +0530

    Update NullTransformer.java

--- NullTransformer.java
+++ NullTransformer.java
@@ -12,18 +12,36 @@ import soot.toolkits.graph.*;
 import soot.toolkits.scalar.BackwardFlowAnalysis;
 import soot.toolkits.scalar.FlowSet;
 
-public class NullTransformer extends SceneTransformer {
-    static CallGraph cg;
+public class NullTransformer extends BodyTransformer {
     @Override
-    protected void internalTransform(String arg0, Map<String, String> arg1) {
-        Set<SootMethod> methods = new HashSet <>();
-        cg = Scene.v().getCallGraph();
-        // Get the main method
-        SootMethod mainMethod = Scene.v().getMainMethod();
+    protected void internalTransform(Body body, String phaseName, Map<String, String> options) {
+        // Get the control flow graph 
+        UnitGraph cfg = new BriefUnitGraph(body);
+
+        // Get live local using Soot's exiting analysis
+        LiveLocals liveLocals = new SimpleLiveLocals(cfg);
+
+        // get function units
+        PatchingChain<Unit> units = body.getUnits();
+
+        HashMap<Unit, ArrayList<JAssignStmt>> newStmts = new HashMap<>();
+        for (Unit u: units){
+            newStmts.put(u, new ArrayList<>());
+            List<Local> before = liveLocals.getLiveLocalsBefore(u);
+            List<Local> after = liveLocals.getLiveLocalsAfter(u);
+            List<Local> unused = new ArrayList<>(); 
+            for (Local l: before){
+                if (!after.contains(l)){
+                    unused.add(l);
+                }
+            }     
+            for (Local l: unused){
+                JAssignStmt newUnit = new JAssignStmt(l, NullConstant.v());
+                newStmts.get(u).add(newUnit);
+            }
+        }
+        for (Unit u: newStmts.keySet()){
+            units.insertAfter(newStmts.get(u), u);
+        }
     }
 }
-
-
-
-
-    
